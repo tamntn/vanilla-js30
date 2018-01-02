9f4c34ec11665db93e7e75c3e1d2b961c5dfe530
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-01-02 00:13:46 -0600

    Test Day 20

--- 20 - Speech Detection/scripts.js
+++ 20 - Speech Detection/scripts.js
@@ -9,7 +9,7 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-    const transcipt = Array.from(e.results)
+    const transcript = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
         .join('');
